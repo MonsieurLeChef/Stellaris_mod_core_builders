d08f993d556c09d20f62a557a5ac2617308e8cfb
MonsieurLeChef <99559405+MonsieurLeChef@users.noreply.github.com>
2024-07-19 00:17:28 +0900

    Update test_core_species.py

--- tests/test_core_species.py
+++ tests/test_core_species.py
@@ -1,3 +1,13 @@
 def test_species_name():
     species_name = "Core Species"
     assert species_name == "Core Species"
+    
+def test_start_system():
+    system_name = "Core System"
+    assert system_name == "Core System"
+
+def test_hyperlane():
+    hyperlane_from = "Core System"
+    hyperlane_to = "Random System"
+    assert hyperlane_from == "Core System"
+    assert hyperlane_to == "Random System"
